f9822ca20d9fb37def4ffed57b9e16dc730b7830
Aidan Rolfe <aidanrolfe@outlook.com>
2026-02-13 13:23:02 +0000

    Sell Tokens Example

--- examples/subscription/sell_tokens.ts
+++ examples/subscription/sell_tokens.ts
@@ -1,68 +1,98 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Connection, Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
-import { AnchorProvider, Wallet } from "@coral-xyz/anchor";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+import { AnchorProvider, Program } from "@coral-xyz/anchor";
 import {
   TOKEN_PROGRAM_ID,
+  TOKEN_2022_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
   getOrCreateAssociatedTokenAccount,
+  getAssociatedTokenAddressSync,
 } from "@solana/spl-token";
-import fs from "fs";
-import { KEYPAIR_PATH, RPC_ENDPOINT, TOKEN_MINT, TxOracleIDL } from "../../config";
+import { Txoracle } from "../../types/txoracle";
+import idl from "../../idl/txoracle.json";
 
-const SELL_AMOUNT_TOKENS = 1000;
+const USDT_MINT = new PublicKey(
+  idl.constants.find((c) => c.name === "USDT_MINT")!.value as string
+);
+const SUBSCRIPTION_TOKEN_MINT = new PublicKey(
+  idl.constants.find((c) => c.name === "TXLINE_MINT")!.value as string
+);
 
 async function main() {
-  const userKeypair = Keypair.fromSecretKey(
-    new Uint8Array(JSON.parse(fs.readFileSync(KEYPAIR_PATH, "utf8")))
-  );
+  console.log("Starting subscription token sale for USDT");
 
-  const connection = new Connection(RPC_ENDPOINT, "confirmed");
-  const wallet = new Wallet(userKeypair);
-  const provider = new AnchorProvider(connection, wallet, {
-    commitment: "confirmed",
-  });
+  const provider = AnchorProvider.env();
   anchor.setProvider(provider);
-  const program = new anchor.Program(TxOracleIDL, provider);
 
-  console.log(`Using user wallet: ${userKeypair.publicKey.toBase58()}`);
+  const program = new Program<Txoracle>(idl as Txoracle, provider);
 
-
-  const userTokenAccount = await getOrCreateAssociatedTokenAccount(
-    connection,
-    userKeypair,
-    TOKEN_MINT,
-    userKeypair.publicKey
-  );
-  console.log("User Token Account:", userTokenAccount.address.toBase58());
-
-  const [solTreasuryPda] = PublicKey.findProgramAddressSync(
-    [Buffer.from("sol_treasury")],
+  const [usdtTreasuryPda] = PublicKey.findProgramAddressSync(
+    [Buffer.from("usdt_treasury")],
     program.programId
   );
 
-  const [tokenTreasuryVaultPda] = PublicKey.findProgramAddressSync(
-    [Buffer.from("token_treasury")],
+  const [tokenTreasuryPda] = PublicKey.findProgramAddressSync(
+    [Buffer.from("token_treasury_v2")],
     program.programId
   );
 
-  const amountOfTokens = new anchor.BN(SELL_AMOUNT_TOKENS);
+  const sellerUsdtAccount = await getOrCreateAssociatedTokenAccount(
+    provider.connection,
+    provider.wallet.payer as any,
+    USDT_MINT,
+    provider.wallet.publicKey
+  );
 
-  console.log(`Selling ${SELL_AMOUNT_TOKENS} tokens...`);
+  const sellerTokenAccount = await getOrCreateAssociatedTokenAccount(
+    provider.connection,
+    provider.wallet.payer as any,
+    SUBSCRIPTION_TOKEN_MINT,
+    provider.wallet.publicKey,
+    false,
+    "confirmed",
+    undefined,
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  const usdtTreasuryVault = getAssociatedTokenAddressSync(
+    USDT_MINT,
+    usdtTreasuryPda,
+    true,
+    TOKEN_PROGRAM_ID
+  );
+
+  const tokenTreasuryVault = getAssociatedTokenAddressSync(
+    SUBSCRIPTION_TOKEN_MINT,
+    tokenTreasuryPda,
+    true,
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  const tokenAmount = new anchor.BN(1_000_000); // 1 token (assuming 6 decimals)
+
+  console.log(`Selling ${tokenAmount.toNumber() / 1_000_000} subscription tokens for USDT...`);
 
   const txSignature = await program.methods
-    .sellSubscriptionToken(amountOfTokens)
+    .sellSubscriptionTokenUsdt(tokenAmount)
     .accounts({
-      seller: userKeypair.publicKey,
-      solTreasuryPda: solTreasuryPda,
-      tokenTreasuryVault: tokenTreasuryVaultPda,
-      sellerTokenAccount: userTokenAccount.address,
-      subscriptionTokenMint: TOKEN_MINT,
-      systemProgram: SystemProgram.programId,
+      seller: provider.wallet.publicKey,
+      usdtMint: USDT_MINT,
+      sellerUsdtAccount: sellerUsdtAccount.address,
+      usdtTreasuryVault: usdtTreasuryVault,
+      usdtTreasuryPda: usdtTreasuryPda,
+      subscriptionTokenMint: SUBSCRIPTION_TOKEN_MINT,
+      tokenTreasuryVault: tokenTreasuryVault,
+      tokenTreasuryPda: tokenTreasuryPda,
+      sellerTokenAccount: sellerTokenAccount.address,
       tokenProgram: TOKEN_PROGRAM_ID,
+      token2022Program: TOKEN_2022_PROGRAM_ID,
+      associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+      systemProgram: SystemProgram.programId,
     })
-    .signers([userKeypair])
     .rpc();
 
   console.log("Transaction Signature:", txSignature);
+  console.log(`View on Solana Explorer: https://explorer.solana.com/tx/${txSignature}?cluster=devnet`);
 }
 
 if (require.main === module) {
