82273edf86f3adad86480c02d3bb3003991feac9
Alex Kozlenkov <alex.kozlenkov@googlemail.com>
2026-06-30 13:54:04 +0100

    NOJIRA Begin re-adding the examples

---
+++ examples/devnet/common/config.ts
@@ -0,0 +1,11 @@
+export const API_BASE_URL = "https://txline-dev.txodds.com/api";
+
+export const JWT_URL = "https://txline-dev.txodds.com/auth/guest/start";
+
+export const TOKEN_DECIMALS = 6;
+
+export const durationInSeconds = 300;
+
+export const currentTs = Math.floor(Date.now() / 1000);
+
+export const subscriptionEndTs = currentTs + durationInSeconds;
